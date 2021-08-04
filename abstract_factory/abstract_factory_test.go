004f49fe5ea7f679fee3e111a7285cc3ebfaa618
kiki-ki <malpie0619@gmail.com>
2021-08-05 01:59:18 +0900

    fix test

--- abstract_factory/abstract_factory_test.go
+++ abstract_factory/abstract_factory_test.go
@@ -2,32 +2,28 @@ package absf
 
 import "testing"
 
-func TestListFactory(t *testing.T) {
-	f := New("list")
-	page := setPage(f)
-	result := page.makeHTML()
-	expect := "<html>\n<head><title>MyPage</title></head>\n<body>\n<h1>MyPage</h1><ul><li>\nContents\n<ul>\n<li><a href=\"https://github.com/kiki-ki\">Github</a></li>\n</ul>\n</li>\n</ul><hr><adress>kiki-ki</adress></body>\n</html>\n"
-	if result != expect {
-		t.Errorf("Expect result to %s, but %s\n", expect, result)
+func TestAbstractFactory(t *testing.T) {
+	expecteds := map[string]string{
+		"list":  "<html>\n<head><title>MyPage</title></head>\n<body>\n<h1>MyPage</h1><ul><li>\nContents\n<ul>\n<li><a href=\"https://github.com/kiki-ki\">Github</a></li>\n</ul>\n</li>\n</ul><hr><adress>kiki-ki</adress></body>\n</html>\n",
+		"table": "<html>\n<head><title>MyPage</title></head>\n<body>\n<h1>MyPage</h1><table width=\"80%\" border=\"3\">\n<tr><td>\n<table width=\"100%\" border=\"1\"><tr>\n<td bgcolor=\"#cccccc\" algin=\"center\" colsapn=\"1\"><b>Contents</b></td>\n</tr>\n<tr>\n<td><a href=https://github.com/kiki-ki>Github</a></td>\n</tr></table>\n</td>\n</tr></table><hr><adress>kiki-ki</adress></body>\n</html>\n",
 	}
+	t.Run("list", func(t *testing.T) {
+		testAbstractFactory(t, New("list"), expecteds["list"])
+	})
+	t.Run("table", func(t *testing.T) {
+		testAbstractFactory(t, New("table"), expecteds["table"])
+	})
 }
 
-func TestTableFactory(t *testing.T) {
-	f := New("table")
-	page := setPage(f)
-	page.Output(page)
-	result := page.makeHTML()
-	expect := "<html>\n<head><title>MyPage</title></head>\n<body>\n<h1>MyPage</h1><table width=\"80%\" border=\"3\">\n<tr><td>\n<table width=\"100%\" border=\"1\"><tr>\n<td bgcolor=\"#cccccc\" algin=\"center\" colsapn=\"1\"><b>Contents</b></td>\n</tr>\n<tr>\n<td><a href=https://github.com/kiki-ki>Github</a></td>\n</tr></table>\n</td>\n</tr></table><hr><adress>kiki-ki</adress></body>\n</html>\n"
-	if result != expect {
-		t.Errorf("Expect result to %s, but %s\n", expect, result)
-	}
-}
-
-func setPage(f factory) page {
+func testAbstractFactory(t *testing.T, f factory, expected string) {
 	link := f.CreateLink("Github", "https://github.com/kiki-ki")
 	tray := f.CreateTray("Contents")
 	tray.Add(link)
 	page := f.CreatePage("MyPage", "kiki-ki")
 	page.Add(tray)
-	return page
+	result := page.makeHTML()
+
+	if result != expected {
+		t.Errorf("Expected result to %s, but %s\n", expected, result)
+	}
 }
